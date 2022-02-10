5c5654c42215e2d025ffabedac2641ea59138b77
Mykola Zhabko <kokozhansei@gmail.com>
2022-02-10 23:19:56 +0200

    Added validation and StrngTrimmer

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,10 +1,28 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	//add an initBinder to convert trim input strings
+	//remove leading and trailing whitespace
+	//resolve issues for our validation
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimerEditor);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
@@ -12,5 +30,20 @@ public class CustomerController {
 		model.addAttribute("customer", new Customer());
 		
 		return "customer-form";
+	}
+	
+	
+	@RequestMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult theBindingResult) {
+		
+		System.out.println("LastName: | " + customer.getLastName() + "|");
+		if(theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else
+		{
+			return "customer-confirmation";
+	
+		}
 	}
 }
